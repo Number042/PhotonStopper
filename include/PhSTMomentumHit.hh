9dba32ea294ac78247182d166f38a345d7e5f2ea
Number042 <marian.lueckhof@cern.ch>
2017-06-08 15:53:15 +0200

    Files uploaded

---
+++ include/PhSTMomentumHit.hh
@@ -0,0 +1,54 @@
+/*
+ * PhSTMomentumHit.hh
+ *
+ *  Created on: Jun 6, 2017
+ *      Author: mlueckhof
+ */
+
+#ifndef PhSTMomentumHit
+#define PhSTMomentumHit
+
+#include "G4Allocator.hh";
+#include "G4THitsCollection.hh";
+#include "G4VHit.hh";
+#include "G4ThreeVector.hh";
+
+class G4AttDef;
+class G4AttValue;
+
+class PhSTMomentumHit : public G4VHit {
+
+public:
+
+	// Constructor
+	//
+	PhSTMomentumHit(G4ThreeVector momentum);
+
+	// Destructor
+	//
+	virtual ~PhSTMomentumHit();
+	inline void *operator new(size_t);
+	inline void operator delete(void *aHit);
+
+	// Methods
+	//
+	virtual void Print();
+
+	inline const G4ThreeVector& GetMomentum() const { return trackMomentum; }
+
+private:
+
+	G4ThreeVector trackMomentum;
+};
+
+typedef G4THitsCollection<PhSTMomentumHit> PhSTMomentumHitsCollection;
+
+extern G4Allocator<PhSTMomentumHit> PhSTMomentumHitAllocator;
+
+inline void* PhSTMomentumHit::operator new(size_t) {
+	void* aHit;
+	aHit = (void)
+}
+
+
+#endif /* INCLUDE_PHSTMOMENTUMHIT_HH_ */
---
+++ src/PhSTEdepHit.cc
@@ -0,0 +1,20 @@
+/*
+ * PhSTEdepHit.cc
+ *
+ *  Created on: Jun 6, 2017
+ *      Author: mlueckhof
+ */
+
+#include "PhSTEdepHit.hh"
+
+G4Allocator<PhSTEdepHit> PhSTEdepHitAllocator;
+
+PhSTEdepHit::PhSTEdepHit() :
+		fDepositedEnergy(0.0) {}
+
+PhSTEdepHit::~PhSTEdepHit() {}
+
+void PhSTEdepHit::Print() {}
+
+
+
